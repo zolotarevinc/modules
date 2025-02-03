cbb6600d10857072fdb43ab027baad677b01a1dd
zolotarevinc <138712753+zolotarevinc@users.noreply.github.com>
2025-02-03 22:45:45 +0300

    Add files via upload

---
+++ married.py
@@ -0,0 +1,47 @@
+# meta pic: https://static.whypodg.me/mods!irisfarm.png
+# meta banner: https://mods.whypodg.me/badges/irisfarm.jpg
+# meta developer: @zolotarevxc
+# scope: hikka_only
+# scope: hikka_min 1.2.10
+
+from .. import loader, utils
+import asyncio
+
+class FarmdMod(loader.Module):
+    """Модуль для фарма колец в @MarriedGroupBot 🌘 ʕ·ᴥ· ʔ"""
+
+    strings = {
+        "name": "FarmdMod",
+        "description": "Модуль для фарма колец в @MarriedGroupBot 🌘 ʕ·ᴥ· ʔ",
+        "developer": "Разработчик: @zolotarevxc 🌟"
+    }
+
+    def __init__(self):
+        self.running = False 
+
+    async def farmdoncmd(self, message):
+        """Включить автоматическую отправку /job 🚀"""
+        if self.running:
+            await message.edit("<b>❎Процесс уже запущен!</b>")
+            return
+
+        self.running = True
+        await message.edit("<b>✅Автоматическая отправка /job запущена!</b>")
+
+        while self.running:
+            await message.client.send_message(message.chat.id, "/job")
+            await asyncio.sleep(1800)  # 30 минут
+
+    async def farmdoffcmd(self, message):
+        """Отключить автоматическую отправку /job ⚡️"""
+        if not self.running:
+            await message.edit("<b>❎Процесс не запущен!</b>")
+            return
+
+        self.running = False
+        await message.edit("<b>❎Автоматическая отправка /job остановлена!</b>")
+
+    async def on_load(self):
+        """При загрузке модуля выводим описание и информацию о разработчике"""
+        await utils.answer(message, f"{self.strings['description']}\n{self.strings['developer']}")
+
